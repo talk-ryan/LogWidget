9f1cb5679fafd85e84330d799294ea9a4f53f8c4
Ryan Talk <rtalk@rsimail.com>
2016-08-26 10:02:21 -0700

    - Add some error handling to disconnect a subprocess if things go wrong. - Parse multiple filenames separated by comma in the Config file

--- LogMain.py
+++ LogMain.py
@@ -1,4 +1,5 @@
-"""This file will contain the main function and serve as the starting point for the application"""
+"""This file will contain the main function and serve as the starting point for the application.
+When this file is run as main, zipped logged files should be created and stored in C:\ZippedLogs"""
 import os
 import zipfile
 import configparser
@@ -7,7 +8,8 @@ import paramiko
 from smb.SMBConnection import SMBConnection
 import tempfile
 import subprocess
-import ConfigurationUtility as cu
+import ConfigurationUtility as Cu
+import sys
 import shutil
 from datetime import datetime
 from datetime import date
@@ -23,7 +25,7 @@ def create_directory(path):
 
 def zip_logs(log_files):
     """Zips the log_files and stores them in the default location"""
-    zip_file = zipfile.ZipFile(r'c:\Test\Test2\UltimateTest.zip', 'w')
+    zip_file = zipfile.ZipFile(r'c:\Test\Test2\UltimateTest2.zip', 'w')
     for file in log_files:
         zip_file.write(file)
     zip_file.close()
@@ -33,17 +35,24 @@ def zip_logs(log_files):
 # TODO Connect to the host and be able to run the get_log_files method
 def test_shutil():
     """Test method that Zips the logs from a remote server"""
-    network_path = r'\\10.1.1.242'
+    network_path = r'\\10.1.1.226'
     user = 'qauser@qa.rsi'
     password = 'test@qa'
-    connect_to_network = 'NET USE ' + network_path + ' /User:' + user + ' ' + password
-    subprocess.Popen(connect_to_network, stdout=subprocess.PIPE, shell=True)
-    if os.path.exists(network_path + r'\RSI\Logs\BRE\WebService.log.2016-08-22(AM)'):
-        print("It exists")
-    files = cu.get_log_files(network_path)
-    zip_logs(files)
-    disconnect_from_network = 'NET USE ' + network_path + ' /delete'
-    subprocess.Popen(disconnect_from_network, stdout=subprocess.PIPE, shell=True)
+    try:
+        connect_to_network = 'NET USE ' + network_path + ' /User:' + user + ' ' + password
+        subprocess.Popen(connect_to_network, stdout=subprocess.PIPE, shell=True)
+        if os.path.exists(network_path + r'\RSI\Logs\BRE\WebService.log.2016-08-22(AM)'):
+            print("It exists")
+        files = Cu.get_log_files(network_path)
+        zip_logs(files)
+        disconnect_from_network = 'NET USE ' + network_path + ' /delete'
+        subprocess.Popen(disconnect_from_network, stdout=subprocess.PIPE, shell=True)
+
+    except subprocess.SubprocessError:
+        disconnect_from_network = 'NET USE ' + network_path + ' /delete'
+        subprocess.Popen(disconnect_from_network, stdout=subprocess.PIPE, shell=True)
+        e = sys.exc_info()
+        print(e)
     # shutil.copy2(network_path + r'\RSI\Logs\BRE\WebService.log.2016-08-22(AM)', r'C:\test.log')
     pass
 
@@ -52,15 +61,12 @@ def main():
     """This is the start of the application"""
     print('In Main')
     create_directory("c:\ZippedLogs")
-    logs = get_log_files()
-    zip_file = zip_logs(logs)
-    return zip_file
+    test_shutil()
     pass
 
 if __name__ == '__main__':
     # test_shutil()
-    get_log_time('10.1.1.242')
     # test_paramiko()
     # get_log_time()
     # get_log_files()
-    # main()
+    main()
